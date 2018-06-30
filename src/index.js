c7aac0cecb2104f58d3dec28d4a4029020c2bb0e
Johnny Packard <packard81@me.com>
2018-06-30 12:40:12 -0500

    added react middleware

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+// redux
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'redux-logger';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
