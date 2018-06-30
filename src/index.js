e1aae443e535e23d4a0621dcbaa739836f89c7e5
Johnny Packard <packard81@me.com>
2018-06-30 12:58:07 -0500

    added feedback reducer to index

--- src/index.js
+++ src/index.js
@@ -5,8 +5,36 @@ import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 // redux
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import {Provider} from 'redux-logger';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'react-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Feedback{
+    constructor(feeling, comprehension, support, comments){
+        this.feeling = feeling,
+        this.comprehension = comprehension,
+        this.support = support,
+        this.comments = comments
+    }
+}
+
+const feedbackReducer = (state = {allFeedback: [], selected: []}, action) => {
+    if(action.type === 'ADD_FEEDBACK'){
+        return new Feedback(action.payload.feeling, action.payload.comprehension,
+            action.payload.support, action.payload.comments);
+    }
+    else if (action.type === 'DELETE_FEEDBACK') {
+        const feedbackToRemove = feedback = feedback._id !== action.payload;
+        return {...state, selected: state.selected.filter(feedbackToRemove)};
+    }
+    else if (action.type === 'SHOW_ALL'){
+        return  {...state, allFeedback: [...action.payload]}
+    }
+    return state;
+}
+// end feedbackReducer
+
+
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
